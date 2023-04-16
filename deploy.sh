8c96458872e519b35ee90dd2766a5734019feae0
Gui Xiang <276935962@qq.com>
2023-04-16 23:33:20 +0800

    修改 deploy.sh 文件

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,13 @@ set -e
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:Wastast/learn-webgis.git
 
